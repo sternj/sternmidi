d50b9d60a18649c9c6e3cda5126f7cbd797813e9
sternj <binaryman00010@gmail.com>
2017-12-06 14:47:06 -0500

    Update multi-faders.ino

--- Projects/multi-faders.ino
+++ Projects/multi-faders.ino
@@ -6,7 +6,7 @@ double faderMax = 0;
 double faderMin = 0;
 int  motorMappings[9][3] = {{22,23,24},{25,26,27},{28,29,30},{31,32,33},{34,35,36},{37,38,39},{40,41,42},{43,44,45},{46,47,48}};
 double motorCalibration[9][2];
-
+int storedValues[9];
 void calibrateFader() {
   //pin order-- white, blue, green
     for(int i = 0; i < 9; i++) {
@@ -14,19 +14,16 @@ void calibrateFader() {
       digitalWrite(motorMappings[i][0], HIGH);
       delay(250);
        digitalWrite(motorMappings[i][0], LOW);
-       motorCalibration[i][0] = analogRead(i);
+       motorCalibration[i][1] = analogRead(i);
        digitalWrite(motorMappings[i][1], HIGH);
       delay(250);
        digitalWrite(motorMappings[i][1], LOW);
-       motorCalibration[i][1] = analogRead(i);
+       motorCalibration[i][0] = analogRead(i);
+      digitalWrite(motorMappings[i][0],LOW);
+       storedValues[i] = motorCalibration[i][1] + (motorCalibration[i][1]-motorCalibration[i][0])/2;
     }
 }
 void setup() {
-  pinMode(A0, INPUT);
-  pinMode(2,OUTPUT);
-pinMode(3, OUTPUT);
-pinMode(4, INPUT);
-pinMode(5, INPUT);
   Serial.begin(9600);
   calibrateFader();
   // begins to read value from serial monitor
@@ -35,35 +32,29 @@ pinMode(5, INPUT);
 }
 
 void loop() {
-
- //   Serial.println(analogRead(A0));
-    if(digitalRead(5) == HIGH) {
-      Serial.print("Black button pressed\n");
-      String str = "Got ";
       
-      
-      
-      gotten = analogRead(A0);
-      str += gotten;
-      str += "\n";
-      Serial.print(str);
-    }
-    if(digitalRead(4) == HIGH) {
-      Serial.print("Red button pressed\n");
-       String str = "Got=ing to ";
-      str += gotten;
-      str += "\n";
-      //adapted from codyhazelwood/motorized-fader-arduino
-      Serial.print(str);
-      
-    }
-
+      for(int i = 0; i < 9; ++i) {
+          if(abs(storedValues[i]-analogRead(i)) >= 10) {
+            adjustFaderToValue(motorMappings[i],i,storedValues[i],i,motorCalibration[i];
+            delay(10);
+          } 
+      }
    
   }
-void adjustFaderToValue(int potNum, int newValue, int analogPin, double[] maxAndMin) {
+void adjustFaderToValue(int[] pins, int potNum, int newValue, int analogPin, double[] maxAndMin) {
   //form of pins-- [upPin, downPin, enablePin]
   //form of maxAndMin-- [min,max]
   if (newValue < analogRead(analogPin) - 10 && gotten > maxAndMin[0]) {
+        digitalWrite(pins[1], HIGH);
+        while (gotten < analogRead(analogPin) - 10) {};  //Loops until motor is done moving
+        digitalWrite(pins[1], LOW);
+    }
+    else if (newValue > analogRead(analogPin) + 10 && gotten < maxAndMin[1]) {
+        digitalWrite(pins[0], HIGH);
+        while (gotten > analogRead(analogPin) + 10) {}; //Loops until motor is done moving
+        digitalWrite(pins[0], LOW);
+    }
+     if (newValue < analogRead(analogPin) - 10 && gotten > maxAndMin[0]) {
         digitalWrite(pins[1], HIGH);
         while (gotten < analogRead(analogPin) - 10) {};  //Loops until motor is done moving
         digitalWrite(pins[1], LOW);
